464fd0c9c1efe64640c0282403b09c5122859453
Toby Koschny <tk@miha-bodytec.de>
2025-07-09 09:20:51 +0200

    Add Header and favicon

---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,10 @@
+<header
+	class="bg-offwhite fixed top-0 z-15 col-span-12 flex h-24 w-full items-center justify-around p-5 shadow-md md:justify-between"
+>
+	<div class="hidden flex-1 md:block"></div>
+
+	<p class="font-poppins text-center text-xl leading-6 font-bold tracking-tight md:text-2xl">
+		Activate<br /> Your Business!
+	</p>
+	<div class="flex flex-1 justify-end"></div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Header from '$lib/components/layout/Header.svelte';
 	import '../app.css';
 
 	let { children } = $props();
@@ -7,9 +8,8 @@
 <div
 	class="from-ice to-offwhite sticky min-h-screen bg-gradient-to-tr bg-cover bg-fixed bg-no-repeat"
 >
+	<Header></Header>
 	<div id="twelve-grid" class="bg-offwhite max-w-8xl mx-auto grid grid-cols-12">
 		{@render children()}
 	</div>
 </div>
-
-{@render children()}
